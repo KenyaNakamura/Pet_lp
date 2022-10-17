de3d609a5307dd0c7293c51b8740ac9a33685019
DESKTOP-L5I0AIV\nanami <komesunanami@gmail.com>
2022-10-17 19:54:50 +0900

    no message

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 // 各セクションのフェードイン実装
-$(function(){
-    $(window).on('load scroll',function (){
-      $('.animation').each(function(){
-        //ターゲットの位置を取得
-        var target = $(this).offset().top;
-        //スクロール量を取得
-        var scroll = $(window).scrollTop();
-        //ウィンドウの高さを取得
-        var height = $(window).height();
-        //ターゲットまでスクロールするとフェードインする
-        if (scroll > target - height){
-          //クラスを付与
-          $(this).addClass('active');
-        }
-      });
+$(function () {
+  $(window).on('load scroll', function () {
+    $('.animation').each(function () {
+      //ターゲットの位置を取得
+      var target = $(this).offset().top;
+      //スクロール量を取得
+      var scroll = $(window).scrollTop();
+      //ウィンドウの高さを取得
+      var height = $(window).height();
+      //ターゲットまでスクロールするとフェードインする
+      if (scroll > target - height) {
+        //クラスを付与
+        $(this).addClass('active');
+      }
     });
   });
+});
 
 
 
-// 
-  AOS.init({
-offset:300,
-duration:200,
-anchorPlacement:"bottom-center",
+AOS.init({
+  offset: 300,
+  duration: 200,
+  anchorPlacement: "bottom-center",
+
+
+  disable: function () {
+    return window.innerWidth < 1000;
+  }
+
 
 });
 
-// kvスライダー
+
+
 
